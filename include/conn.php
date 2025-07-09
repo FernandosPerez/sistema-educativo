736eefd80b2b6790543cc7a2c6fb869b393a86ca
FernandosPerez <fernandoestrp@outlook.es>
2025-07-09 14:33:08 -0600

    Se modifica el archivo docker compose y docker file

--- include/conn.php
+++ include/conn.php
@@ -1,8 +1,8 @@
 <?php
 	$host = 'db';
-	$main_db_name ='database';
-	$main_db_username ='user';
-	$main_db_password ='pass#';  
+	$main_db_name ='boostrap';
+	$main_db_username ='user1';
+	$main_db_password ='user1.pa55';  
 
 	$dbconn = new PDO('mysql:host='.$host.';dbname='.$main_db_name.'',$main_db_username,$main_db_password);
 	$dbconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
